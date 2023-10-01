efbfa602ff7134d55d98c732e7c4481801b1b833
Simo680 <erik.simonyan2005@gmail.com>
2023-10-01 14:58:44 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './components/TodoList'; 
+import { useState } from "react";
+import AddTodo from './components/AddTodo';
+
+
+
 
 function App() {
+  
+  let data = [
+    {id: 1, title: 'Позавтракать', completed: true},
+    {id: 2, title: 'Почистить огурцы', completed: false},
+    {id: 3, title: 'Помыть кота', completed: true},
+  ]
+
+    const [todos, setTodos] = useState(data)
+  
+    function removeTodo(id) {
+      let removeTasks = todos.filter((elem => elem.id !== id))
+      setTodos(removeTasks)
+    }
+
+    function changeTodo(id){
+      let changeTasks = todos.map(elem => {
+        if (elem.id === id) {
+          elem.completed = !elem.completed
+        }
+        return elem
+      })
+      setTodos(changeTasks)
+    }
+
+    function addNewTodo(title) {
+      let newTodo = {
+        id: Date.now(),
+        completed: false,
+        title
+      } 
+      setTodos([...todos, newTodo])
+    }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+    <AddTodo addNewTodo={addNewTodo}/>
+    <TodoList 
+    todos={todos} 
+              removeTodo={removeTodo}
+              changeTodo={changeTodo}/>
+    
+  </div>
+  )
 }
 
 export default App;
